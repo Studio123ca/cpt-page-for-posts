9bdf6c524434a88b7191f54ba2b8b305f7ab33e0
Cody Marcoux <cody@studio123.ca>
2023-12-06 11:23:04 -0500

    Clean up

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Custom Post Type Archive Page Selector
-Description: This plugin lets you easily choose a specific Page to serve as the main archive page for your custom post types. It works just like how you set the Posts page for your regular blog posts.
+Description: Select a specific Page as the main archive page for custom post types.
 Version: 1.0.0
 Author: Studio123
 Author URI: https://studio123.ca
 */
 
 // Register the setting for each post type
-function register_post_type_page_for_posts_settings()
+function register_post_type_page_settings()
 {
     $post_types = get_post_types(['has_archive' => true], 'objects');
 
@@ -16,7 +16,7 @@ function register_post_type_page_for_posts_settings()
         register_setting('custom_post_type_page_for_posts', 'cpt_page_for_posts_' . $post_type->name);
     }
 }
-add_action('admin_init', 'register_post_type_page_for_posts_settings');
+add_action('admin_init', 'register_post_type_page_settings');
 
 // Add settings sections and fields
 function add_settings_sections()
@@ -28,23 +28,24 @@ function add_settings_sections()
             'cpt_page_for_posts_section_' . $post_type->name,
             '',
             function () use ($post_type) {
-                display_post_type_page_for_posts_fields($post_type->name);
+                display_post_type_page_fields($post_type->name);
             },
             'custom_post_type_page_for_posts'
         );
     }
 }
+add_action('admin_init', 'add_settings_sections');
 
-// Create an admin page where you can set the page associations for each post type
-function custom_post_type_page_for_posts_page()
+// Create an admin page for setting page associations
+function custom_post_type_page_page()
 {
 ?>
-    <div class="wrap">
-        <h1>Custom Post Type Archive Pages</h1>
+    <div class="wrap" id="poststuff">
+        <h1><?php esc_html_e('Custom Post Type Archive Pages', 'cpt-page-for-posts'); ?></h1>
 
-        <p>This plugin allows you to select a Page top act as the archive page for each of your custom post types, similar to how you can set a Page to act as the Posts page for your regular blog posts.</p>
+        <p><?php esc_html_e('This plugin allows you to select a Page to act as the archive page for each custom post type, similar to setting a Page for regular blog posts.', 'cpt-page-for-posts'); ?></p>
 
-        <p><strong>Note:</strong> This plugin is tailored for custom post types with the <code>has_archive</code> parameter set to <code>true</code>. They will not show up in the list below if they do not have this parameter set.</p>
+        <p><strong><?php esc_html_e('Note:', 'cpt-page-for-posts'); ?></strong> <?php esc_html_e('This plugin is tailored for custom post types with the has_archive parameter set to true.', 'cpt-page-for-posts'); ?></p>
 
         <form method="post" action="options.php">
             <?php
@@ -56,36 +57,39 @@ function custom_post_type_page_for_posts_page()
     </div>
 <?php
 }
+add_action('admin_menu', 'load_page_for_posts_settings_page');
 
-// Display the settings fields for associating a page with each post type on the admin page
-function display_post_type_page_for_posts_fields($post_type)
+// Display the settings fields for associating a page with each post type
+function display_post_type_page_fields($post_type)
 {
     $option_name = 'cpt_page_for_posts_' . $post_type;
     $current_value = get_option($option_name);
     $pages = get_pages();
     $post_type_object = get_post_type_object($post_type);
 
-    // sort pages alphabetically
+    // Sort pages alphabetically
     usort($pages, function ($a, $b) {
         return strcmp($a->post_title, $b->post_title);
     });
 ?>
     <table class="form-table">
-        <tr>
-            <th scope="row">
-                <label for="<?php echo esc_attr($option_name); ?>">
-                    <?php echo esc_html($post_type_object->labels->singular_name); ?>
-                </label>
-            </th>
-            <td>
-                <select id="<?php echo esc_attr($option_name); ?>" name="<?php echo esc_attr($option_name); ?>">
-                    <option value="0" <?php selected($current_value, 0); ?>>None (Default)</option>
-                    <?php foreach ($pages as $page) : ?>
-                        <option value="<?php echo esc_attr($page->ID); ?>" <?php selected($current_value, $page->ID); ?>><?php echo esc_html($page->post_title); ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </td>
-        </tr>
+        <tbody>
+            <tr>
+                <th scope="row">
+                    <label for="<?php echo esc_attr($option_name); ?>">
+                        <?php echo esc_html($post_type_object->labels->singular_name); ?>
+                    </label>
+                </th>
+                <td>
+                    <select id="<?php echo esc_attr($option_name); ?>" name="<?php echo esc_attr($option_name); ?>">
+                        <option value="0" <?php selected($current_value, 0); ?>><?php esc_html_e('None (Default)', 'cpt-page-for-posts'); ?></option>
+                        <?php foreach ($pages as $page) : ?>
+                            <option value="<?php echo esc_attr($page->ID); ?>" <?php selected($current_value, $page->ID); ?>><?php echo esc_html($page->post_title); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </td>
+            </tr>
+        </tbody>
     </table>
 <?php
 }
@@ -94,15 +98,14 @@ function display_post_type_page_for_posts_fields($post_type)
 function load_page_for_posts_settings_page()
 {
     add_options_page(
-        'Custom Post Type Archive Pages',
-        'CPT Archive Pages',
+        esc_html__('Custom Post Type Archive Pages', 'cpt-page-for-posts'),
+        esc_html__('CPT Archive Pages', 'cpt-page-for-posts'),
         'manage_options',
         'custom_post_type_page_for_posts',
-        'custom_post_type_page_for_posts_page'
+        'custom_post_type_page_page'
     );
 }
 add_action('admin_menu', 'load_page_for_posts_settings_page');
-add_action('admin_init', 'add_settings_sections');
 
 function get_cpt_page_for_posts($post_type)
 {
