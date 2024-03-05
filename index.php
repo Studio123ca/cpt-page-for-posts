ea3b883b1cf5d8a7bf24df5a38203da9c378d3d8
Cody Marcoux <cody@studio123.ca>
2024-03-05 14:54:27 -0500

    Fixes, UI changes
    * Adds statuses to Page list in the admin panel.
    
    * Prefix all functions for safety. :construction_worker:
    
    * Display notice when editing an archive page.

--- index.php
+++ index.php
@@ -2,29 +2,39 @@
 /*
 Plugin Name: Custom Post Type Archive Pages
 Description: Select a specific Page as the main archive page for custom post types.
-Version: 1.1.0
+Version: 1.2.0
 Author: Studio123
 Author URI: https://studio123.ca
 */
 
+// Main function - Get the page ID for a post type archive
+function cpt_page_for_posts($post_type)
+{
+    if (empty($post_type)) {
+        return false;
+    }
+
+    return get_option('cpt_page_for_posts_' . $post_type);
+}
+
 // Register the setting for each post type
-function register_post_type_page_settings()
+function cpfp_register_post_type_page_settings()
 {
     $post_types = get_post_types(['has_archive' => true], 'objects');
 
     foreach ($post_types as $post_type) {
-        register_setting('custom_post_type_page_for_posts', 'cpt_page_for_posts_' . $post_type->name);
+        register_setting('cpfp_custom_post_type_page_for_posts', 'cpt_page_for_posts_' . $post_type->name);
     }
 
     // Register the setting for the checkbox
-    register_setting('custom_post_type_page_for_posts', 'cpt_page_for_posts_enable_uninstall_cleanup');
+    register_setting('cpfp_custom_post_type_page_for_posts', 'cpt_page_for_posts_enable_uninstall_cleanup');
 
     add_option('cpt_page_for_posts_enable_uninstall_cleanup', 1);
 }
-add_action('admin_init', 'register_post_type_page_settings');
+add_action('admin_init', 'cpfp_register_post_type_page_settings');
 
 // Add settings sections and fields
-function add_settings_sections()
+function cpfp_add_settings_sections()
 {
     $post_types = get_post_types(['has_archive' => true], 'objects');
 
@@ -36,40 +46,40 @@ function add_settings_sections()
         'cpt_page_for_posts_section_cpt_archives',
         '',
         function () use ($post_types) {
-            display_post_type_page_fields($post_types);
+            cpfp_display_post_type_page_fields($post_types);
         },
-        'custom_post_type_page_for_posts'
+        'cpfp_custom_post_type_page_for_posts'
     );
 
     add_settings_section(
         'cpt_page_for_posts_section_uninstall',
         'Plugin Settings',
-        'display_uninstall_checkbox',
-        'custom_post_type_page_for_posts'
+        'cpfp_display_uninstall_checkbox',
+        'cpfp_custom_post_type_page_for_posts'
     );
 }
-add_action('admin_init', 'add_settings_sections');
+add_action('admin_init', 'cpfp_add_settings_sections');
 
 // Create an admin page for setting page associations
-function custom_post_type_page_page()
+function cpfp_custom_post_type_page_page()
 {
 ?>
     <div class="wrap">
         <h1><?php esc_html_e('Custom Post Type Archive Pages', 'cpt-page-for-posts'); ?></h1>
         <form method="post" action="options.php" style="margin-top: 20px" class="metabox-holder">
             <?php
-            settings_fields('custom_post_type_page_for_posts');
-            do_settings_sections('custom_post_type_page_for_posts');
+            settings_fields('cpfp_custom_post_type_page_for_posts');
+            do_settings_sections('cpfp_custom_post_type_page_for_posts');
             submit_button();
             ?>
         </form>
     </div>
 <?php
 }
-add_action('admin_menu', 'load_page_for_posts_settings_page');
+add_action('admin_menu', 'cpfp_load_page_for_posts_settings_page');
 
 // Display the settings fields for associating a page with each post type
-function display_post_type_page_fields($post_types)
+function cpfp_display_post_type_page_fields($post_types)
 { ?>
     <div id="cpt_archive_pages_metabox" class="postbox" style="margin-bottom: 50px">
         <div class="postbox-header">
@@ -130,7 +140,7 @@ function display_post_type_page_fields($post_types)
 <?php
 }
 
-function display_uninstall_checkbox()
+function cpfp_display_uninstall_checkbox()
 {
     $option_name = 'cpt_page_for_posts_enable_uninstall_cleanup';
     $enabled = get_option($option_name, 0);
@@ -156,17 +166,17 @@ function display_uninstall_checkbox()
 }
 
 // Load the settings page
-function load_page_for_posts_settings_page()
+function cpfp_load_page_for_posts_settings_page()
 {
     add_options_page(
         esc_html__('Custom Post Type Archive Pages', 'cpt-page-for-posts'),
         esc_html__('CPT Archive Pages', 'cpt-page-for-posts'),
         'manage_options',
-        'custom_post_type_page_for_posts',
-        'custom_post_type_page_page'
+        'cpfp_custom_post_type_page_for_posts',
+        'cpfp_custom_post_type_page_page'
     );
 }
-add_action('admin_menu', 'load_page_for_posts_settings_page');
+add_action('admin_menu', 'cpfp_load_page_for_posts_settings_page');
 
 // Uninstall cleanup
 function custom_post_type_page_uninstall()
@@ -185,30 +195,63 @@ function custom_post_type_page_uninstall()
     }
 }
 
-// Get the page ID for a post type archive
-function cpt_page_for_posts($post_type)
-{
-    if (empty($post_type)) {
-        return false;
-    }
-
-    return get_option('cpt_page_for_posts_' . $post_type);
-}
-
 // Add post state to the page list table
-function add_post_state($post_states, $post)
+add_filter('display_post_states', 'cpfp_add_post_state', 10, 2);
+function cpfp_add_post_state($post_states, $post)
 {
     $post_types = get_post_types(['has_archive' => true], 'objects');
+    $archive_names = array();
 
     foreach ($post_types as $post_type) {
         $option_name = 'cpt_page_for_posts_' . $post_type->name;
         $page_id = get_option($option_name);
 
         if ($page_id == $post->ID) {
-            $post_states['cpt_page_for_posts'] = $post_type->labels->singular_name . ' Archive';
+            $archive_names[] = $post_type->labels->name . ' Page';
         }
+    }
+
+    if (!empty($archive_names)) {
+        $post_states['cpt_page_for_posts'] = implode(', ', $archive_names);
     }
 
     return $post_states;
 }
-add_filter('display_post_states', 'add_post_state', 10, 2);
+
+// Display a message above the content editor but below the title when editing the archive page
+add_action('edit_form_after_title', 'cpfp_display_edit_page_message');
+
+function cpfp_display_edit_page_message($post)
+{
+    $post_types = get_post_types(['has_archive' => true], 'objects');
+    $archive_names = array();
+    $admin_page_link = admin_url('options-general.php?page=cpfp_custom_post_type_page_for_posts');
+
+    foreach ($post_types as $post_type) {
+        $option_name = 'cpt_page_for_posts_' . $post_type->name;
+        $page_id = get_option($option_name);
+
+        if ($page_id == $post->ID) {
+            $archive_names[] = $post_type->labels->name;
+        }
+    }
+
+    // display notice for each
+    if (!empty($archive_names)) {
+        foreach ($archive_names as $archive_name) {
+            echo '<div class="notice notice-warning is-dismissible inline"><p>';
+            printf(
+                __('You are currently editing the page that shows your latest %s.', 'cpt-page-for-posts'),
+                esc_html(strtolower($archive_name))
+            );
+            
+            printf(
+                ' <a href="%s" style="text-decoration:none;">%s</a>',
+                esc_url($admin_page_link),
+                __('<span class="dashicons dashicons-info" style="transform:scale(0.8)"></span>', 'cpt-page-for-posts')
+            );
+            echo '</p>';
+            echo '</div>';
+        }
+    }
+};
