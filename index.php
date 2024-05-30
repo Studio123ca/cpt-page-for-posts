b670f8fdfbd6e48f00a365d6ae5961d248f5b25c
Cody Marcoux <cody@studio123.ca>
2024-05-30 09:34:37 -0400

    Version bump

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Post Type Archive Pages
 Description: Select a specific Page as the main archive page for custom post types.
-Version: 1.3.2
+Version: 1.3.3
 Author: Studio123
 Author URI: https://studio123.ca
 */
