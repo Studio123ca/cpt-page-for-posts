067cb82b151056c429785f451ff48fd5965a64ef
Cody Marcoux <cody@studio123.ca>
2025-06-19 19:13:44 -0400

    Add archive slug override

--- inc/acf.php
+++ inc/acf.php
@@ -5,69 +5,64 @@
  */
 
 // Use ACF_Location to add archive pages to the field group locations conditional
-class ACF_Location_Archive_Pages extends ACF_Location
-{
+class ACF_Location_Archive_Pages extends ACF_Location {
 
-    public function initialize()
-    {
-        $this->name = 'archive_page';
-        $this->label = __('Archive Page', 'acf');
-        $this->category = 'page';
-        $this->object_type = 'post';
-    }
+	public function initialize() {
+		$this->name = 'archive_page';
+		$this->label = __( 'Archive Page', 'acf' );
+		$this->category = 'page';
+		$this->object_type = 'post';
+	}
 
-    public function get_values($rule)
-    {
-        $choices = array();
-        $post_types = get_post_types(['has_archive' => true], 'objects');
+	public function get_values( $rule ) {
+		$choices = array();
+		$post_types = get_post_types( [ 'has_archive' => true ], 'objects' );
 
-        foreach ($post_types as $post_type) {
-            $post_type_object = get_post_type_object($post_type->name);
-            $option_slug = $post_type->name;
-            $option_label = $post_type_object->labels->name . ' Page';
+		foreach ( $post_types as $post_type ) {
+			$post_type_object = get_post_type_object( $post_type->name );
+			$option_slug = $post_type->name;
+			$option_label = $post_type_object->labels->name . ' Page';
 
-            $choices[$option_slug] = $option_label;
-        }
+			$choices[ $option_slug ] = $option_label;
+		}
 
-        return $choices;
-    }
+		return $choices;
+	}
 
-    public function match($rule, $screen, $field_group)
-    {
-        if (isset($screen['post_id'])) {
-            $post_id = $screen['post_id'];
-            $archive_page_id = get_option('cpt_page_for_posts_' . $rule['value']);
+	public function match( $rule, $screen, $field_group ) {
+		if ( isset( $screen['post_id'] ) ) {
+			$post_id = $screen['post_id'];
+			$archive_page_id = get_option( 'cpt_page_for_posts_' . $rule['value'] );
 
-            // WPML compatibility
-            if (class_exists('SitePress')) {
-                $archive_page_id = icl_object_id($archive_page_id, 'page', true);
-            }
+			// WPML compatibility
+			if ( class_exists( 'SitePress' ) ) {
+				$archive_page_id = icl_object_id( $archive_page_id, 'page', true );
+			}
 
-            if ($post_id == $archive_page_id) {
-                if ($rule['operator'] == '==') {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                if ($rule['operator'] == '!=') {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
+			if ( $post_id == $archive_page_id ) {
+				if ( $rule['operator'] == '==' ) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				if ( $rule['operator'] == '!=' ) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 }
 
 // Add the location to the ACF Location rules
-add_action('acf/init', 'my_acf_init_location_types');
-function my_acf_init_location_types()
-{
-    // Check function exists, then include and register the custom location type class.
-    if (function_exists('acf_register_location_type')) {
-        acf_register_location_type('ACF_Location_Archive_Pages');
-    }
+add_action( 'acf/init', 'my_acf_init_location_types' );
+function my_acf_init_location_types() {
+	// Check function exists, then include and register the custom location type class.
+	if ( function_exists( 'acf_register_location_type' ) ) {
+		acf_register_location_type( 'ACF_Location_Archive_Pages' );
+	}
 }
--- index.php
+++ index.php
@@ -2,31 +2,30 @@
 /*
 Plugin Name: Custom Post Type Archive Pages
 Description: Select a specific Page as the main archive page for custom post types.
-Version: 1.3.3
+Version: 1.4.0
 Author: Studio123
 Author URI: https://studio123.ca
 */
 
 // Includes
-require_once('inc/functions.php');
+require_once( 'inc/functions.php' );
 
-if (function_exists('acf_add_options_page')) {
-    require_once('inc/acf.php');
+if ( function_exists( 'acf_add_options_page' ) ) {
+	require_once( 'inc/acf.php' );
 }
 
 // Main function - Get the page ID for a post type archive
-function cpt_page_for_posts($post_type)
-{
-    if (empty($post_type)) {
-        return false;
-    }
+function cpt_page_for_posts( $post_type ) {
+	if ( empty( $post_type ) ) {
+		return false;
+	}
 
-    $post_id = get_option('cpt_page_for_posts_' . $post_type);
+	$post_id = get_option( 'cpt_page_for_posts_' . $post_type );
 
-    // WPML compatibility
-    if (class_exists('SitePress')) {
-        $post_id = apply_filters( 'wpml_object_id', $post_id, 'page' );
-    }
+	// WPML compatibility
+	if ( class_exists( 'SitePress' ) ) {
+		$post_id = apply_filters( 'wpml_object_id', $post_id, 'page' );
+	}
 
-    return $post_id;
+	return $post_id;
 }
